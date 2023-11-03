a343d93cbb3c45c5cf7d77531459bfcfd1ab32c4
Dev <kanuj2151@gmail.com>
2023-11-03 09:56:25 +0530

    3.0

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -72,14 +72,13 @@ public class BankAccount {
 
     public void deposit(double amount) {
         //add amount to balance
-        if(amount > 0)
-            balance += amount;
+        balance += amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-        if(balance - amount < minBalance)
+        if((balance - amount) >= minBalance)
             throw new Exception("Insufficient Balance");
         else
             balance -= amount;

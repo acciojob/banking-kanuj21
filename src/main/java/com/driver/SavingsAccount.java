0fb2c1cb1463ab286e584e6eadb70b6889ed7f08
Dev <kanuj2151@gmail.com>
2023-11-05 00:14:15 +0530

    0000

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -45,9 +45,7 @@ public class SavingsAccount extends BankAccount{
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
 
-        //p + p * r * t /100
-        // p(1 + (r* t) /100)
-        return getBalance() + (getBalance() * rate * years / 100);
+        return getBalance() * (1 + (rate * years) / 100);
 
     }
 
